2dae1070df3f7ce6cf22e0d0cda4913a334ad669
Seongbin Kim <seongbin9786@gmail.com>
2018-12-10 16:57:18 +0900

    INIT

---
+++ src/main/java/io/toast/ToastBackendApplication.java
@@ -0,0 +1,12 @@
+package io.toast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToastBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/io/toast/ToastBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package io.toast;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToastBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
